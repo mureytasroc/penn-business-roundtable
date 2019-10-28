39a9e8e64af1dace0f5fde12c97a1cba6a21544f
Charles Cunningham <mureytasroc@gmail.com>
2019-10-28 13:27:18 -0400

    Emails

--- public/js/pennbusinessroundtable.js
+++ public/js/pennbusinessroundtable.js
@@ -3,3 +3,7 @@ document.getElementById(document.title).className += " active"
 $(function () {
 $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
+
+function speakerBack(){
+  window.history.back()
+}
